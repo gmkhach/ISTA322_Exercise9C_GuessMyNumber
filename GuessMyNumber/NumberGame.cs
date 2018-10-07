5795c39c9d18f8a8f0358e9e8af3020fd0e9198b
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-06 23:15:23 -0400

    created an class of NumberGame and moved the related method into that class

---
+++ GuessMyNumber/NumberGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumber
+{
+    class NumberGame
+    {
+        public NumberGame(int[] list, int number)
+        {
+            this.list = list;
+            this.number = number;
+        }
+
+        private int[] list;
+        private int number;
+
+        public void FindValue(int[] list)
+        {
+            int midValue = list.Length % 2 == 0 ? list[list.Length / 2 - 1] : list[list.Length / 2];
+            if (number == midValue)
+            {
+                Console.WriteLine($"\nValue is {midValue}");
+            }
+            else
+            {
+                Console.WriteLine($"\nValue is " + (number > midValue ? "greater" : "less") + $" than {midValue}");
+                List<int> newList = new List<int>();
+                int startIndex = number > midValue ? Array.IndexOf(list, midValue) + 1 : 0;
+                int endIndex = number < midValue ? Array.IndexOf(list, midValue) - 1 : list.Length - 1;
+                for (int i = startIndex; i <= endIndex; i++)
+                {
+                    newList.Add(list[i]);
+                }
+                FindValue(newList.ToArray());
+            }
+        }
+
+    }
+}
--- GuessMyNumber/Program.cs
+++ GuessMyNumber/Program.cs
@@ -10,33 +10,17 @@ namespace GuessMyNumber
     {
         static void Main(string[] args)
         {
+            Run();
+        }
+
+        static void Run()
+        {
             int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             Console.Write("\nEnter an integer between 1 and 10\n\n>>> ");
-            int value = int.Parse(Console.ReadLine().Trim());
-            FindValue(list, value);
-
+            int number = int.Parse(Console.ReadLine().Trim());
+            NumberGame game = new NumberGame(list, number);
+            game.FindValue(list);
         }
 
-        static void FindValue(int[] list, int value)
-        {
-            int midValue = list.Length % 2 == 0 ? list[list.Length / 2 - 1] : list[list.Length / 2] ;
-            if (value == midValue)
-            {
-                Console.WriteLine($"\nValue is {midValue}");
-            }
-            else
-            {
-                Console.WriteLine($"\nValue is " + (value > midValue ? "greater" : "less") + $" than {midValue}");
-                List<int> newList = new List<int>();
-
-                int startIndex = value > midValue ? Array.IndexOf(list, midValue) + 1 : 0;
-                int endIndex = value < midValue ? Array.IndexOf(list, midValue) - 1 : list.Length - 1;
-                for (int i = startIndex; i <= endIndex; i++)
-                {
-                    newList.Add(list[i]);
-                }
-                FindValue(newList.ToArray(), value);
-            }
-        }
     }
 }
